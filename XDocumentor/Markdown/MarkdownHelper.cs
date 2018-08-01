9e82890fdd377f9138c50cd6479b7ffcf2fb55df
Alexey Drapash <drapash@elibrary.ru>
2018-08-01 13:29:17 +0300

    First commit

---
+++ XDocumentor/Markdown/MarkdownHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace XDocumentor.Markdown
+{
+    static class MarkdownHelper
+    {
+        public static string RenderType(Type t, bool isFull = false)
+        {
+            if (t == null) return "";
+            if (t == typeof(void)) return "void";
+            if (!t.IsGenericType) return (isFull) ? t.FullName : t.Name;
+
+            var innerFormat = string.Join(", ", t.GetGenericArguments().Select(x => RenderType(x)));
+            return Regex.Replace(isFull ? t.GetGenericTypeDefinition().FullName : t.GetGenericTypeDefinition().Name, @"`.+$", "") + "<" + innerFormat + ">";
+        }
+
+        public static string RenderMethodInfo(MethodInfo methodInfo)
+        {
+            var isExtension = methodInfo.GetCustomAttributes<System.Runtime.CompilerServices.ExtensionAttribute>(false).Any();
+
+            var seq = methodInfo.GetParameters().Select(x =>
+            {
+                var suffix = x.HasDefaultValue ? (" = " + (x.DefaultValue ?? $"null")) : "";
+                return "`" + RenderType(x.ParameterType) + "` " + x.Name + suffix;
+            });
+
+            return methodInfo.Name + "(" + (isExtension ? "this " : "") + string.Join(", ", seq) + ")";
+        }
+
+        public static string RenderCode(string code)
+        {
+            return "`" + code + "`";
+        }
+    }
+}
---
+++ XDocumentor/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using XDocumentor.Markdown;
+
+namespace XDocumentor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var target = "Debug/Elibrary.Builder.dll";
+            string dest = "Debug/md";
+            MarkdownGenerateMode generateMode = MarkdownGenerateMode.FilePerClass;
+            string namespaceMatch = string.Empty;
+            if (args.Length == 1)
+            {
+                target = args[0];
+            }
+            else if (args.Length == 2)
+            {
+                target = args[0];
+                dest = args[1];
+            }
+            else if (args.Length == 3)
+            {
+                target = args[0];
+                dest = args[1];
+                generateMode = Enum.Parse<MarkdownGenerateMode>(args[2]);
+            }
+            else if (args.Length == 4)
+            {
+                target = args[0];
+                dest = args[1];
+                generateMode = Enum.Parse<MarkdownGenerateMode>(args[2]);
+                namespaceMatch = args[3];
+            }
+
+            var types = MarkdownGenerator.BuildFrom(target, namespaceMatch);
+
+            if (types.Length == 0)
+                return;
+
+            if (!Directory.Exists(dest)) Directory.CreateDirectory(dest);
+            else
+            {
+                var directory = new DirectoryInfo(dest);
+                foreach (FileInfo file in directory.GetFiles()) file.Delete();
+                foreach (DirectoryInfo subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+            }
+
+            var homeBuilder = new MarkdownBuilder();
+            homeBuilder.Header(1, "Contents");
+            homeBuilder.AppendLine();
+            
+            switch (generateMode)
+            {
+                case MarkdownGenerateMode.FilePerClass:
+                    GenerateFilePerClass(homeBuilder, dest, types);
+                    break;
+                case MarkdownGenerateMode.FilePerNamespace:
+                    GenerateFilePerNamespace(homeBuilder, dest, types);
+                    break;
+                default:
+                    throw new Exception("Unknown MarkdownGenerateMode");
+            }
+
+            File.WriteAllText(Path.Combine(dest, "Home.md"), homeBuilder.ToString());
+        }
+
+        private static void GenerateFilePerClass(MarkdownBuilder homeBuilder, string dest, MarkdownableType[] types)
+        {
+            foreach (var g in types.GroupBy(x => x.Namespace).OrderBy(x => x.Key))
+            {
+                string namespacePath = Path.Combine(dest, g.Key);
+
+                homeBuilder.Header(2, g.Key);
+                homeBuilder.AppendLine();
+
+                if (!Directory.Exists(namespacePath)) Directory.CreateDirectory(namespacePath);
+                
+                foreach (var item in g.OrderBy(x => x.Name))
+                {
+                    string renderedDisplayName = item.DisplayName.Replace("<", "").Replace(">", "").Replace(",", "").Replace(" ", "-");
+                    var sb = new StringBuilder();
+                    homeBuilder.ListLink(MarkdownHelper.RenderCode(item.DisplayName), g.Key + "/" + renderedDisplayName);
+                    sb.Append(item.ToString());
+
+                    File.WriteAllText(Path.Combine(namespacePath, renderedDisplayName + ".md"), sb.ToString());
+                }
+                                
+                homeBuilder.AppendLine();
+            }
+        }
+
+        private static void GenerateFilePerNamespace(MarkdownBuilder homeBuilder, string dest, MarkdownableType[] types)
+        {
+            foreach (var g in types.GroupBy(x => x.Namespace).OrderBy(x => x.Key))
+            {
+                homeBuilder.HeaderWithLink(2, g.Key, g.Key);
+                homeBuilder.AppendLine();
+
+                var sb = new StringBuilder();
+                foreach (var item in g.OrderBy(x => x.Name))
+                {
+                    homeBuilder.ListLink(MarkdownHelper.RenderCode(item.DisplayName), g.Key + "#" + item.DisplayName.Replace("<", "").Replace(">", "").Replace(",", "").Replace(" ", "-").ToLower());
+
+                    sb.Append(item.ToString());
+                }
+
+                File.WriteAllText(Path.Combine(dest, g.Key + ".md"), sb.ToString());
+                homeBuilder.AppendLine();
+            }
+        }
+    }
+}
